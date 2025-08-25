d9f4ed7ac1bdd62dcb4393a24c84d99a7119c88b
Narendra Mannan <narenman7@gmail.com>
2025-08-24 21:26:04 -0700

    feat: add Sentry monitoring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from '@sentry/react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './state/Store';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
