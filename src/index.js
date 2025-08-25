0cc41e815add3fe33ae7aa578b2354bb56ec778e
Narendra Mannan <narenman7@gmail.com>
2025-08-24 22:44:38 -0700

    fix: remove duplicate Sentry import and init

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './state/Store';
-import * as Sentry from '@sentry/react';
 
 // Support both SENTRY_DSN and REACT_APP_SENTRY_DSN for CRA compatibility
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.REACT_APP_SENTRY_DSN;
 if (SENTRY_DSN) {
   Sentry.init({ dsn: SENTRY_DSN });
-}
-
-const dsn = process.env.REACT_APP_SENTRY_DSN || process.env.SENTRY_DSN;
-if (dsn) {
-  Sentry.init({ dsn });
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
